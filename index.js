00cdfa331b4671c5e9d679dea74007267279dada
karthik <sampathiraomalleswarao123@gmail.com>
2026-04-28 17:54:29 +0530

    remove the text

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Storing data and elements into variables
+
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
@@ -8,15 +8,15 @@ const tabBtn = document.getElementById("tab-btn");
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-//To prevent the leads from disappearing when the page is refreshed or when our browser is closed
+
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
-//Making our SAVE TAB button work
+
 tabBtn.addEventListener("click", function() {
-    //grabbing the url of the current tab
+    
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         myLeads.push(tabs[0].url)
         localStorage.setItem("myLeads", JSON.stringify(myLeads))
@@ -24,8 +24,7 @@ tabBtn.addEventListener("click", function() {
     })
 })
 
-//Rendering out the leads in the unordered list, using for loop and template strings
-//This function is called in multiple places
+
 function render(leads) {
     let listItems = "";
     for (let i = 0; i < leads.length; i++) {
@@ -41,19 +40,19 @@ function render(leads) {
     ulEl.innerHTML = listItems;
 }
 
-//Making our DELETE BUTTON work so it clears all leads from the local storage when clicked
+
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
     render(myLeads);
 })
 
-//Pushing the values from the input field
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
-    //to clear out the input field after saving a lead
+     
     inputEl.value = "";
-    //To save the leads to the local storage
+   
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
     render(myLeads);
 })
