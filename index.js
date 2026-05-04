80167c6d4dae2149b6efc8768b8ad329dac9b0a8
karthik <sampathiraomalleswarao123@gmail.com>
2026-05-04 14:58:23 +0530

    remove the extra part

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ tabBtn.addEventListener("click", function() {
 function render(leads) {
     let listItems = "";
     for (let i = 0; i < leads.length; i++) {
-    // using template strings
+    
     listItems += `
         <li>
             <a target='_blank' href='${leads[i]}'>
